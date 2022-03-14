7916304e4bc57b8da01428ef43d7becfefcd8463
Sergio Troche <sergio.troche@ucb.edu.bo>
2022-03-14 17:23:54 -0400

    Todo OK

--- server.js
+++ server.js
@@ -34,16 +34,17 @@ app.get('/tweets/:user', async (req, res) => {
 // Buscar los tweets de un usuario en particular
 app.get('/alltweets', async (req, res) => {
   try {
+  const pgClientT = new Pool()
   const redisData = await get('alltweetsRD')
   if (redisData) {
     return res.json({source: 'redis', data: JSON.parse(redisData)})
   }
-  const data = await pgClient.query('select * from tweets')
+  const data = await pgClientT.query('select * from tweets')
   await set('alltweetsRD', JSON.stringify(data.rows), 'EX', 10)
   res.json({source: 'pg', data: data.rows});
   
   }catch (err) {
-  console.error(`Error while getting quotes `, err.message);
+  console.error('Error while getting quotes', err.message);
   res.status(err.statusCode || 500).json({'message': err.message});
   }
 
@@ -84,7 +85,7 @@ app.get('/users', async (req, res) => {
   }
 
 });
-// 
+// funciona
 // Crear un nuevo tweet 
 app.post("/ptweet/:usuario/:textos", async (req, res) => {
   try {
@@ -99,14 +100,15 @@ app.post("/ptweet/:usuario/:textos", async (req, res) => {
   }
   
 });
-// 
+// funciona
 // realizar un follow de un usuario a otro
 app.post("/pfollow/:idfollower/:idfolloweed", async (req, res) => {  
   try {  
     var idFA = Number(req.params.idfollower);
     var idFB = Number(req.params.idfolloweed);
-  const dataP = await pgClient.query('INSERT INTO follows (id_follower,id_followeed) VALUES ($1, $2)', [idFA,idFB]);
-  res.json({source: 'pg', dataP: dataP.rows});  
+    const pgClientFO = new Pool()
+    await pgClientFO.query('INSERT INTO follows (id_follower,id_followeed) VALUES ($1, $2)', [idFA,idFB]);
+    res.status(pgClientPT.statusCode || 200).json({'message': 'datos ingresados correctamente'}); 
   } catch (err) {
   console.error(`Error while posting quotes `, err.message);
   res.status(err.statusCode || 500).json({'message': err.message});
